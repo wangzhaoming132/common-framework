edd5d95daf5f7025225765ae3a81d78d14d6a540
wangzhaoming132@qq.com <wangzhaoming132@qq.com>
2020-01-10 11:31:22 +0800

    测试11112

--- src/main/java/com/web/commonframework/CommonframeworkApplication.java
+++ src/main/java/com/web/commonframework/CommonframeworkApplication.java
@@ -4,8 +4,10 @@ package com.web.commonframework;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CommonframeworkApplication {
 
     public static void main(String[] args) {
